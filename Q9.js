ad41522b49c37b516a4ce361fe5a4b4a9e5b5d16
Habiba-Wasim <160347457+Habiba-Wasim@users.noreply.github.com>
2024-02-23 21:02:33 +0500

    Step9 Of Typescript
    Step9 Of Typescript

---
+++ Q9.js
@@ -0,0 +1,4 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let num = 4987;
+console.log(`My Favourite Number Is ${num}`);
---
+++ Q9.ts
@@ -0,0 +1,4 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let num: number = 4987
+console.log(`My Favourite Number Is ${num}`);
